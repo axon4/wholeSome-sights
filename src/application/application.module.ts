61d0894ac9f276315b243eb393c8d9dce2c05791
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-23 19:00:24 +0100

    generate user-module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ApplicationRoutingModule } from './application-routing.module';
 import { ApplicationComponent } from './application.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
 	declarations: [ApplicationComponent],
 	imports: [
 		BrowserModule,
-		ApplicationRoutingModule
+		ApplicationRoutingModule,
+		UserModule
 	],
 	providers: [],
 	bootstrap: [ApplicationComponent]
