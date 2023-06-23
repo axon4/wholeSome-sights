1d48d04ee4864d0ecf06c3f62d556db6202b276b
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-23 20:09:47 +0100

    generate header-component

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ApplicationRoutingModule } from './application-routing.module';
 import { ApplicationComponent } from './application.component';
 import { UserModule } from './user/user.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-	declarations: [ApplicationComponent],
+	declarations: [
+		ApplicationComponent,
+		HeaderComponent
+	],
 	imports: [
 		BrowserModule,
 		ApplicationRoutingModule,
