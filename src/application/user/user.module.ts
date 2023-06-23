620191ccd0490f3a283c18b935e49aae5c6c4b37
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-23 21:02:35 +0100

    port authentication-modal inTo master-modal with content-projection

--- src/application/user/user.module.ts
+++ src/application/user/user.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModalComponent } from './authentication-modal/authentication-modal.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [AuthenticationModalComponent],
-	imports: [CommonModule],
+	imports: [
+		CommonModule,
+		SharedModule
+	],
 	exports: [AuthenticationModalComponent]
 })
 export class UserModule {};
