63c25df1280c296659364dccde26d6ff7c3aaa20
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-23 20:28:03 +0100

    generate modal-component in shared-module

---
+++ src/application/shared/modal/modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ws-modal',
+	templateUrl: './modal.component.html',
+	styleUrls: ['./modal.component.css']
+})
+export class ModalComponent {};
