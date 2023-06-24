12f4f6290b3501ab13a71414bba7c89cb7399187
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-24 03:43:53 +0100

    generate tabs & tab components in shared-module

--- src/application/shared/shared.module.ts
+++ src/application/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsComponent } from './tab/tabs/tabs.component';
+import { TabComponent } from './tab/tab/tab.component';
 // import { ModalService } from '../modal/modal.service';
 
 @NgModule({
-	declarations: [ModalComponent],
+	declarations: [ModalComponent, TabsComponent, TabComponent],
 	imports: [CommonModule],
-	exports: [ModalComponent]
+	exports: [ModalComponent, TabsComponent, TabComponent]
 	// providers: [ModalService]
 })
 export class SharedModule {};
---
+++ src/application/shared/tab/tabs/tabs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ws-tabs',
+	templateUrl: './tabs.component.html',
+	styleUrls: ['./tabs.component.css']
+})
+export class TabsComponent {};
