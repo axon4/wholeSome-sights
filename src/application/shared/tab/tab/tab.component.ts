7b412ccd3ebf7c0a2a7a71bcccf0f9c05ab09baa
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-24 15:56:55 +0100

    select active tab and set visibility & styles

--- src/application/shared/tab/tab/tab.component.ts
+++ src/application/shared/tab/tab/tab.component.ts
@@ -7,4 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class TabComponent {
 	@Input() title = '';
+	
+	active = false;
 };
--- src/application/shared/tab/tabs/tabs.component.ts
+++ src/application/shared/tab/tabs/tabs.component.ts
@@ -10,6 +10,15 @@ export class TabsComponent implements AfterContentInit {
 	@ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
 	ngAfterContentInit() {
-		console.log(this.tabs);
+		const activeTabs = this.tabs.filter(tab => tab.active);
+
+		if (!activeTabs || (activeTabs.length === 0)) {
+			this.selectTab(this.tabs.first);
+		};
+	};
+
+	selectTab(tab: TabComponent) {
+		this.tabs.forEach(tab => {tab.active = false});
+		tab.active = true;
 	};
 };
