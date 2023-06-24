65bc624038440e4727bf0b247d5516b361cc52df
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-24 15:43:03 +0100

    render tabs from QueryList

--- src/application/shared/tab/tabs/tabs.component.ts
+++ src/application/shared/tab/tabs/tabs.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
 	selector: 'ws-tabs',
 	templateUrl: './tabs.component.html',
 	styleUrls: ['./tabs.component.css']
 })
-export class TabsComponent {};
+export class TabsComponent implements AfterContentInit {
+	@ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+
+	ngAfterContentInit() {
+		console.log(this.tabs);
+	};
+};
