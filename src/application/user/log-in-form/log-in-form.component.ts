6ccdefc21e1b8672178ce261676a7879c6078d5e
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-24 22:30:54 +0100

    generate authentication-forms--components in user-module

---
+++ src/application/user/log-in-form/log-in-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ws-log-in-form',
+	templateUrl: './log-in-form.component.html',
+	styleUrls: ['./log-in-form.component.css']
+})
+export class LogInFormComponent {};
--- src/application/user/user.module.ts
+++ src/application/user/user.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModalComponent } from './authentication-modal/authentication-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { LogInFormComponent } from './log-in-form/log-in-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
-	declarations: [AuthenticationModalComponent],
+	declarations: [AuthenticationModalComponent, LogInFormComponent, RegistrationFormComponent],
 	imports: [
 		CommonModule,
 		SharedModule
