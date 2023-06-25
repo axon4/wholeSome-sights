b1ceb4d25f67d5dab5fc5b45c33290fc9ca50529
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-25 11:20:55 +0100

    create registration reActive--form-group & form-controls, add error-messages under name-inPut

--- src/application/user/registration-form/registration-form.component.ts
+++ src/application/user/registration-form/registration-form.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'ws-registration-form',
 	templateUrl: './registration-form.component.html',
 	styleUrls: ['./registration-form.component.css']
 })
-export class RegistrationFormComponent {};
+export class RegistrationFormComponent {
+	form = new FormGroup({
+		name: new FormControl('', Validators.required),
+		eMail: new FormControl(''),
+		age: new FormControl(''),
+		passWord: new FormControl(''),
+		confirmPassWord: new FormControl('')
+	});
+};
--- src/application/user/user.module.ts
+++ src/application/user/user.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthenticationModalComponent } from './authentication-modal/authentication-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { AuthenticationModalComponent } from './authentication-modal/authentication-modal.component';
 import { LogInFormComponent } from './log-in-form/log-in-form.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
@@ -9,7 +10,8 @@ import { RegistrationFormComponent } from './registration-form/registration-form
 	declarations: [AuthenticationModalComponent, LogInFormComponent, RegistrationFormComponent],
 	imports: [
 		CommonModule,
-		SharedModule
+		SharedModule,
+		ReactiveFormsModule
 	],
 	exports: [AuthenticationModalComponent]
 })
