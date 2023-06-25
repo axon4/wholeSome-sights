b1e257ba96c71157d37539f269cbf013d95dee64
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-26 00:50:13 +0100

    rePlace titleCase-pipe with method to preServe upperCase-letters

--- src/application/shared/inPut/inPut.component.ts
+++ src/application/shared/inPut/inPut.component.ts
@@ -10,4 +10,11 @@ export class InPutComponent {
 	@Input() type = 'text';
 	@Input() placeHolder!: string;
 	@Input() control = new FormControl();
+	
+	// TO-DO: convert to pipe
+	capitaliseFirstLetter(word: string) {
+		const capitalisedWord = word.charAt(0).toUpperCase() + word.slice(1);
+
+		return capitalisedWord;
+	};
 };
