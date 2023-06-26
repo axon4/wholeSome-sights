ebc5d456c3defa1298476ac957536ba00c239f78
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-26 03:23:31 +0100

    generate banner-component in shared-module

--- src/application/shared/shared.module.ts
+++ src/application/shared/shared.module.ts
@@ -7,11 +7,12 @@ import { TabsComponent } from './tab/tabs/tabs.component';
 import { TabComponent } from './tab/tab/tab.component';
 import { InPutComponent } from './inPut/inPut.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-	declarations: [ModalComponent, TabsComponent, TabComponent, InPutComponent],
+	declarations: [ModalComponent, TabsComponent, TabComponent, InPutComponent, BannerComponent],
 	imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
-	exports: [ModalComponent, TabsComponent, TabComponent, InPutComponent],
+	exports: [ModalComponent, TabsComponent, TabComponent, InPutComponent, BannerComponent],
 	providers: [provideEnvironmentNgxMask()]
 })
 export class SharedModule {};
