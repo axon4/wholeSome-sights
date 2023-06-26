74192dde855d1b7df98531798d6b57b40980984e
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-27 00:01:17 +0100

    create user with eMail & passWord using FireBase--authentication-service

--- src/application/user/registration-form/registration-form.component.ts
+++ src/application/user/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,6 +8,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent {
+	constructor(private authentication: AngularFireAuth) {};
+
 	form = new FormGroup({
 		name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(72)]),
 		eMail: new FormControl('', [Validators.required, Validators.email]),
@@ -14,13 +17,28 @@ export class RegistrationFormComponent {
 		passWord: new FormControl('', [Validators.required, /* Validators.pattern(/(.*){7,96}/) */ Validators.minLength(7), Validators.maxLength(96)]),
 		confirmPassWord: new FormControl('', [Validators.required])
 	});
+	pending = false;
 	showBanner = false;
 	bannerMessage = 'Registration in Progress';
 	bannerColour = 'blue';
 
-	submit() {
+	async submit() {
+		this.pending = true;
 		this.showBanner = true;
 		this.bannerMessage = 'Registration in Progress';
 		this.bannerColour = 'blue';
+
+		try {
+			const { name, age, eMail, passWord } = this.form.value;
+
+			await this.authentication.createUserWithEmailAndPassword(eMail as string, passWord as string);
+
+			this.bannerMessage = 'Registration Successful';
+			this.bannerColour = 'green';
+		} catch (error) {
+			this.pending = false;
+			this.bannerMessage = 'Error Registering';
+			this.bannerColour = 'red';
+		};
 	};
 };
