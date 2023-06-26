8b5417bfdfc01ab7f960474e7499db3fd68d9185
axon4 <65050809+axon4@users.noreply.github.com>
2023-06-26 22:39:11 +0100

    initialise FireBase via AngularFire

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
 import { ApplicationRoutingModule } from './application-routing.module';
 import { ApplicationComponent } from './application.component';
 import { UserModule } from './user/user.module';
 import { HeaderComponent } from './header/header.component';
+
+const FireBaseModule = AngularFireModule.initializeApp(environment.fireBase);
 
 @NgModule({
 	declarations: [
@@ -13,7 +18,9 @@ import { HeaderComponent } from './header/header.component';
 	imports: [
 		BrowserModule,
 		ApplicationRoutingModule,
-		UserModule
+		UserModule,
+		FireBaseModule,
+		AngularFireAuthModule
 	],
 	providers: [],
 	bootstrap: [ApplicationComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+	fireBase: {
+		apiKey: 'AIzaSyAPkTmjvPnd7G60IAw_5yDqtuQM6kbRaGA',
+		appId: '1:1083988004656:web:25675fc38d45c55d7c02f5',
+		projectId: 'wholesome-sights',
+		authDomain: 'wholesome-sights.firebaseapp.com',
+		storageBucket: 'wholesome-sights.appspot.com',
+		messagingSenderId: '1083988004656',
+	}
+};
