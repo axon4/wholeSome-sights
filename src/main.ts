91f35fa68edd3cc66c3077018db7e59650530cbc
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-03 16:34:07 +0100

    initialise FireBase beFore Angular

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ApplicationModule } from './application/application.module';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { environment } from './environments/environment';
 
-platformBrowserDynamic()
-	.bootstrapModule(ApplicationModule)
-	.catch(console.error);
+firebase.initializeApp(environment.fireBase);
+
+let applicationInitialised = false;
+
+firebase.auth().onAuthStateChanged(() => {
+	if (!applicationInitialised) {
+		platformBrowserDynamic()
+			.bootstrapModule(ApplicationModule)
+			.catch(console.error);
+			
+		applicationInitialised = true;
+	};
+});
