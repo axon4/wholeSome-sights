d40f9af460694d2d52da74e8e45bdda0dad8ed9c
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-03 22:38:34 +0100

    validate passWord-confirmation with custom validator, add error-message

--- src/application/user/registration-form/registration-form.component.ts
+++ src/application/user/registration-form/registration-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/application/authentication/authentication.service';
+import { RegistrationValidator } from '../validators/registration-validator';
 import User from '../../models/user.model';
 
 @Component({
@@ -17,7 +18,7 @@ export class RegistrationFormComponent {
 		age: new FormControl<number | null>(null, [Validators.required, Validators.min(9), Validators.max(70)]),
 		passWord: new FormControl('', [Validators.required, /* Validators.pattern(/(.*){7,96}/) */ Validators.minLength(7), Validators.maxLength(96)]),
 		confirmPassWord: new FormControl('', [Validators.required])
-	});
+	}, [RegistrationValidator.match('passWord', 'confirmPassWord')]);
 	
 	pending = false;
 	showBanner = false;
--- src/application/user/validators/registration-validator.ts
+++ src/application/user/validators/registration-validator.ts
@@ -1,1 +1,15 @@
-export class RegistrationValidator {};
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class RegistrationValidator {
+	static match(control1Name: string, control2Name: string): ValidatorFn {
+		return function(formGroup: AbstractControl): ValidationErrors | null {
+			const control1 = formGroup.get(control1Name);
+			const control2 = formGroup.get(control2Name);
+			const error = control1?.value === control2?.value ? null : {misMatch: true};
+
+			control2?.setErrors(error);
+	
+			return error;
+		};
+	};
+};
