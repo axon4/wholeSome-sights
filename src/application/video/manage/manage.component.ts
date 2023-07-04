07732b95b3f8eaf10603470d79bd520f956c15f6
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-04 12:48:40 +0100

    generate manage-component in video-module

---
+++ src/application/video/manage/manage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ws-manage',
+	templateUrl: './manage.component.html',
+	styleUrls: ['./manage.component.css']
+})
+export class ManageComponent {};
