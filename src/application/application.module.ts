a2f01b1788876234101aa28fe4f90d79e3931f42
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-04 12:20:32 +0100

    generate home-component

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -5,16 +5,18 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment';
 import { ApplicationRoutingModule } from './application-routing.module';
-import { ApplicationComponent } from './application.component';
 import { UserModule } from './user/user.module';
+import { ApplicationComponent } from './application.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 const FireBaseModule = AngularFireModule.initializeApp(environment.fireBase);
 
 @NgModule({
 	declarations: [
 		ApplicationComponent,
-		HeaderComponent
+		HeaderComponent,
+  		HomeComponent
 	],
 	imports: [
 		BrowserModule,
