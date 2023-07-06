850c3544ef0c0525bd92783dd291c3abf243925e
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-06 16:48:34 +0100

    generate upLoad-component in video-module

---
+++ src/application/video/upLoad/upLoad.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UpLoadComponent } from './upLoad.component';
+
+describe('UpLoadComponent', () => {
+	let component: UpLoadComponent;
+	let fixture: ComponentFixture<UpLoadComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [UpLoadComponent]
+		});
+		fixture = TestBed.createComponent(UpLoadComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
---
+++ src/application/video/upLoad/upLoad.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ws-upLoad',
+	templateUrl: './upLoad.component.html',
+	styleUrls: ['./upLoad.component.css']
+})
+export class UpLoadComponent {};
