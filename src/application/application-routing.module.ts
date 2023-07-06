9414ac99a22884f60d2eee0e278f5e6a3dfd4a9e
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-06 17:50:52 +0100

    add sight-page--temPlate with dynamic-routing

--- src/application/application-routing.module.ts
+++ src/application/application-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SightComponent } from './sight/sight.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: HomeComponent
+	},
+	{
+		path: 'sight/:ID',
+		component: SightComponent
 	}
 ];
 
--- src/application/sight/sight.component.ts
+++ src/application/sight/sight.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'ws-sight',
 	templateUrl: './sight.component.html',
 	styleUrls: ['./sight.component.css']
 })
-export class SightComponent {};
+export class SightComponent {
+	constructor(private route: ActivatedRoute) {
+		this.ID = this.route.snapshot.params.ID;
+	};
+
+	ID: string;
+};
