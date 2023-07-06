99a4d1d92511a10f95412a7db98094f2689cca95
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-06 16:49:20 +0100

    add upLoad-page--temPlate with routing, forMat

--- src/application/video/video-routing.module.ts
+++ src/application/video/video-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UpLoadComponent } from './upLoad/upLoad.component';
 
 const routes: Routes = [
 	{
 		path: 'manage',
 		component: ManageComponent,
+		data: {authenticationRequired: true}
+	},
+	{
+		path: 'upLoad',
+		component: UpLoadComponent,
 		data: {authenticationRequired: true}
 	}
 ];
