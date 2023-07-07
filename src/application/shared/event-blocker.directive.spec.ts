da6e0b154bf7f8d01f49e8e205a5f04e56e2fa63
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-07 12:18:18 +0100

    generate event-blocker--directive in shared-module

---
+++ src/application/shared/event-blocker.directive.spec.ts
@@ -0,0 +1,9 @@
+import { EventBlockerDirective } from './event-blocker.directive';
+
+describe('EventBlockerDirective', () => {
+	it('should create an instance', () => {
+		const directive = new EventBlockerDirective();
+		
+		expect(directive).toBeTruthy();
+	});
+});
--- src/application/shared/shared.module.ts
+++ src/application/shared/shared.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskDirective, provideEnvironmentNgxMask } from 'ngx-mask';
 import { ModalComponent } from './modal/modal.component';
 // import { ModalService } from '../modal/modal.service';
 import { TabsComponent } from './tab/tabs/tabs.component';
 import { TabComponent } from './tab/tab/tab.component';
 import { InPutComponent } from './inPut/inPut.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BannerComponent } from './banner/banner.component';
+import { EventBlockerDirective } from './event-blocker.directive';
 
 @NgModule({
-	declarations: [ModalComponent, TabsComponent, TabComponent, InPutComponent, BannerComponent],
-	imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
-	exports: [ModalComponent, TabsComponent, TabComponent, InPutComponent, BannerComponent],
+	declarations: [
+		ModalComponent,
+		TabsComponent,
+		TabComponent,
+		InPutComponent,
+		BannerComponent,
+		EventBlockerDirective
+	],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		NgxMaskDirective
+	],
+	exports: [
+		ModalComponent,
+		TabsComponent,
+		TabComponent,
+		InPutComponent,
+		BannerComponent,
+		EventBlockerDirective
+	],
 	providers: [provideEnvironmentNgxMask()]
 })
 export class SharedModule {};
