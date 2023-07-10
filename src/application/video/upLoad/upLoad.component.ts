fcba1bf03de521ce820a6f3df4784ab9847ee249
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-10 02:26:47 +0100

    pipe last snapShot to determine upLoad-status, hide progress if not pending

--- src/application/video/upLoad/upLoad.component.ts
+++ src/application/video/upLoad/upLoad.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { last } from 'rxjs';
 import { v4 as UUID } from 'uuid';
 
 @Component({
@@ -15,6 +16,7 @@ export class UpLoadComponent {
 	file: File | null = null;
 	nextStep = false;
 	progress = 0;
+	showProgress = false;
 
 	title = new FormControl('', {
 		validators: [Validators.required, Validators.minLength(3)],
@@ -42,6 +44,7 @@ export class UpLoadComponent {
 		this.showBanner = true;
 		this.bannerMessage = 'UpLoad in Progress';
 		this.bannerColour = 'blue';
+		this.showProgress = true;
 
 		const fileName = `${this.file!.name}-${UUID()}`;
 		const task = this.storage.upload(`sights/${fileName}`, this.file);
@@ -49,5 +52,20 @@ export class UpLoadComponent {
 		task.percentageChanges().subscribe(percentage => {
 			this.progress = (percentage as number) / 100;
 		});
+		task.snapshotChanges()
+			.pipe(last())
+			.subscribe({
+				next: value => {
+					this.bannerMessage = 'UpLoad Successful';
+					this.bannerColour = 'green';
+					this.showProgress = false;
+				},
+				error: error => {
+					this.pending = false;
+					this.bannerMessage = 'Error UpLoading';
+					this.bannerColour = 'red';
+					this.showProgress = false;
+				}
+			});
 	};
 };
