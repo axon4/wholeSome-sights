942679c1ad306280666529531622d93f73a7ba76
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-10 22:20:08 +0100

    generate sight-service

---
+++ src/application/sight/sight.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { SightService } from './sight.service';
+
+describe('SightService', () => {
+	let service: SightService;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(SightService);
+	});
+
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+});
