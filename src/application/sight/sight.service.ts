2ab0bdcce46b5cb3b44ee442cb423e850028dc41
axon4 <65050809+axon4@users.noreply.github.com>
2023-07-18 15:12:05 +0100

    get sights upLoaded by user

--- src/application/sight/sight.service.ts
+++ src/application/sight/sight.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { of, switchMap } from 'rxjs';
 import Sight from 'src/application/models/sight.model';
 
 @Injectable({providedIn: 'root'})
 export class SightService {
-	constructor(private fireStore: AngularFirestore) {
+	constructor(private authentication: AngularFireAuth, private fireStore: AngularFirestore) {
 		this.sightsCollection = fireStore.collection<Sight>('sights');
 	};
 
@@ -12,5 +14,19 @@ export class SightService {
 
 	addSight(sight: Sight) {
 		return this.sightsCollection.add(sight);
+	};
+
+	getSights() {
+		return this.authentication.user.pipe(
+			switchMap(user => {
+				if (!user) {
+					return of([]);
+				} else {
+					const snapShot = this.sightsCollection.ref.where('uID', '==', user.uid).get();
+
+					return snapShot;
+				};
+			})
+		);
 	};
 };
--- src/application/video/manage/manage.component.ts
+++ src/application/video/manage/manage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SightService } from 'src/application/sight/sight.service';
 
 @Component({
 	selector: 'WS-manage',
@@ -7,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 	styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-	constructor(private router: Router, private route: ActivatedRoute) {};
+	constructor(private router: Router, private route: ActivatedRoute, private sight: SightService) {};
 
 	sortOrder: 'newest' | 'oldest' = 'newest';
 
@@ -15,6 +16,7 @@ export class ManageComponent implements OnInit {
 		this.route.queryParams.subscribe(queryParameters => {
 			this.sortOrder = queryParameters.sort === 'oldest' ? queryParameters.sort : 'newest';
 		});
+		this.sight.getSights().subscribe(console.log);
 	};
 
 	sort(event: Event) {
