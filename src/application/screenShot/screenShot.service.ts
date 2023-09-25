edf91c4e1d3252898395b80feeb8860b01306371
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-26 00:23:03 +0100

    get quarterly keyFrames with FFProbe to generate multiple screenShots

--- src/application/screenShot/screenShot.service.ts
+++ src/application/screenShot/screenShot.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { FFprobeWorker } from 'ffprobe-wasm';
 
 @Injectable({providedIn: 'root'})
 export class ScreenShotService {
@@ -21,6 +22,21 @@ export class ScreenShotService {
 		const data = await fetchFile(file);
 
 		this.FFMPEG.FS('writeFile', file.name, data);
-		await this.FFMPEG.run('-i', file.name, '-ss', '00:00:01', '-frames:v', '1', '-filter:v', 'scale=510:-1', 'screenShot1.png');
+
+		const FFProbe = new FFprobeWorker();
+		const { format: { duration } } = await FFProbe.getFileInfo(file);
+
+		FFProbe.terminate();
+
+		const convertSecondsToTime = (seconds: number) => new Date(seconds * 1000).toISOString().slice(11, 19);
+
+		const keyFrames = [Number(duration) / 4, Number(duration) / 2, (Number(duration) / 4) * 3].map(convertSecondsToTime);
+		const commands: string[] = [];
+
+		keyFrames.forEach((keyFrame, index) => {
+			commands.push('-i', file.name, '-ss', keyFrame, '-frames:v', '1', '-filter:v', 'scale=510:-1', `screenShot${index + 1}.png`);
+		});
+
+		await this.FFMPEG.run(...commands);
 	};
 };
