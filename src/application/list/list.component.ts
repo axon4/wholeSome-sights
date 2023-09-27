72ac35724b586590e1d5ddcd0db166b8e5356497
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-27 22:39:16 +0100

    detect when user scrolls to bottom of window

--- src/application/list/list.component.ts
+++ src/application/list/list.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'WS-list',
 	templateUrl: './list.component.html',
 	styleUrls: ['./list.component.css']
 })
-export class ListComponent {};
+export class ListComponent implements OnInit, OnDestroy {
+	ngOnInit() {
+		window.addEventListener('scroll', this.handleScroll);
+	};
+
+	handleScroll = () => {
+		const { innerHeight } = window;
+		const { offsetHeight, scrollTop } = document.documentElement;
+
+		const scrolledToBottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+		if (scrolledToBottomOfWindow) {
+			console.log('bottom');
+		};
+	};
+
+	ngOnDestroy() {
+		window.removeEventListener('scroll', this.handleScroll);
+	};
+};
