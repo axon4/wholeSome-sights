b3d467af50a801651be38bcb8f68bf8a6d7d43ee
axon4 <65050809+axon4@users.noreply.github.com>
2023-10-02 18:11:23 +0100

    initialise VideoJS--video-player, downGrade VideoJS

--- src/application/sight/sight.component.ts
+++ src/application/sight/sight.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import VideoJS from 'video.js';
 
 @Component({
 	selector: 'WS-sight',
 	templateUrl: './sight.component.html',
 	styleUrls: ['./sight.component.css']
 })
-export class SightComponent {
+export class SightComponent implements OnInit {
 	constructor(private route: ActivatedRoute) {
 		this.route.params.subscribe(parameter => {this.ID = parameter.ID});
 	};
 
 	ID = '';
+	@ViewChild('player', {static: true}) target?: ElementRef;
+	player?: VideoJS.Player;
+	
+	ngOnInit() {
+		this.player = VideoJS(this.target?.nativeElement);
+	};
 };
