391afeb5ce1c6a4c34757fdcfd1a49ba4ed10960
axon4 <65050809+axon4@users.noreply.github.com>
2023-10-12 14:12:12 +0100

    write header-tests with mock authentication-service & spy

--- src/application/header/header.component.spec.ts
+++ src/application/header/header.component.spec.ts
@@ -1,20 +1,39 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { HeaderComponent } from './header.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { HeaderComponent } from './header.component';
+import { AuthenticationService } from '../authentication/authentication.service';
 
-// describe('HeaderComponent', () => {
-// 	let component: HeaderComponent;
-// 	let fixture: ComponentFixture<HeaderComponent>;
+describe('HeaderComponent', () => {
+	let fixture: ComponentFixture<HeaderComponent>;
+	let component: HeaderComponent;
+	const mockAuthenticationService = jasmine.createSpyObj('mockAuthenticationService', ['register', 'logOut'], {loggedIn$: of(true)});
 
-// 	beforeEach(() => {
-// 		TestBed.configureTestingModule({
-// 			declarations: [HeaderComponent]
-// 		});
-// 		fixture = TestBed.createComponent(HeaderComponent);
-// 		component = fixture.componentInstance;
-// 		fixture.detectChanges();
-// 	});
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [HeaderComponent],
+			imports: [RouterTestingModule],
+			providers: [{provide: AuthenticationService, useValue: mockAuthenticationService}]
+		});
+		fixture = TestBed.createComponent(HeaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-// 	it('should create', () => {
-// 		expect(component).toBeTruthy();
-// 	});
-// });
+	it('create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('log-out', () => {
+		const element = fixture.debugElement.query(By.css('li:nth-child(4) a'));
+
+		expect(element).toBeTruthy();
+
+		element.triggerEventHandler('click', new MouseEvent('event'));
+
+		const service = TestBed.inject(AuthenticationService);
+
+		expect(service.logOut).toHaveBeenCalledTimes(1);
+	});
+});
